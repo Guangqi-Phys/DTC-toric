9f1c746a7cb6a5425c7ca900bc2ec5e0114a2a2d
Guangqi-Phys <11749027@mail.sustech.edu.cn>
2021-10-22 13:41:51 +0800

    add python decoder

--- py_decoder/tcfunctionsx.py
+++ py_decoder/tcfunctionsx.py
@@ -1,39 +1,29 @@
 import random
 import networkx as nx 
 
+def read_file():
+	with open('data.dat', 'r') as fp:
+		v = fp.readlines()
+		lx = int(v[0])
+		ly = int(v[1])
+		qubits = v[2]
+	er_qubits = [[0 for col in range(lx)] for row in range(2*ly)]
+	m_stablz = [[0 for col in range(lx)] for row in range(ly)]
+	for i_row in range(2*ly):
+		for i_col in range(lx):
+			er_qubits[i_row][i_col] = int(qubits[i_row*lx+i_col])
+	return er_qubits, m_stablz
 
-def torus_initial(lx,ly):
-	""" 
-	Define the initial state of the toric code
-	Input: 
-	  lx: size of column, ly: size of row
-	Output:
-	  stablz: lx*ly matrix of stabilizers with all 0 value
-	  qubits: 2lx*ly matrix of qubits
-	"""
-	stablz = [[0 for col in range(lx)] for row in range(ly)]
-	qubits = [[0 for col in range(lx)] for row in range(2*ly)]
-	return stablz, qubits
 
 
-def torus_error(m_stablz, m_qubits, p):
-	""" 
-	Define the error generating process. In this defination, 
-	we only have Pauli Z error on every qubit with probability p.
-	Input:
-	  Initiallized stablz matrix and qubits matrix of toric code without error.
-	  error probability p
-	Output:
-	  stabilizer and qubits matrix with random errors.
-	"""
-	values = [0,1]
-	prob = [1-p, p]
+def torus_error(m_stablz, er_qubits):
+
 	ly = len(m_stablz)
 	lx = len(m_stablz[0])
+
 	for i_row in range(2*ly):
 	    for i_col in range(lx):
-	        m_qubits[i_row][i_col] = random.choices(values,prob)
-	        if m_qubits[i_row][i_col] == [1]:
+	        if er_qubits[i_row][i_col] == 1:
 	            if i_row % 2 == 0:
 	                stab_row = int(i_row/2)
 	                if stab_row == 0:
@@ -58,8 +48,7 @@ def torus_error(m_stablz, m_qubits, p):
 			else:
 				m_stablz[i_row][i_col] = 1
 	error_stablz = m_stablz
-	error_qubits = m_qubits
-	return error_stablz, error_qubits
+	return error_stablz
 
 
 
@@ -130,99 +119,46 @@ def correct_qubits(matching,er_qubits):
 	    q_row = 2*min(aux_a,aux_c)
 	    if abs(aux_b-aux_d) <= int(lx/2):
 	        for j in range(min(aux_b,aux_d)+1,max(aux_b,aux_d)+1):
-	            crted_qubits[q_row+1][j][0] += 1
+	            crted_qubits[q_row+1][j] += 1
 	    else:
 	        for j in range(0, min(aux_b,aux_d)+1):
-	            crted_qubits[q_row+1][j][0] += 1
+	            crted_qubits[q_row+1][j] += 1
 	        for j in range(max(aux_b,aux_d)+1, lx):
-	            crted_qubits[q_row+1][j][0] += 1
+	            crted_qubits[q_row+1][j] += 1
 	    if aux_a >= aux_c:
 	        q_col = aux_b
 	    else:
 	        q_col = aux_d
 	    if abs(aux_a-aux_c) <= int(ly/2):
 	        for j in range(2*min(aux_a,aux_c)+2,2*max(aux_a,aux_c)+2,2):
-	            crted_qubits[j][q_col][0] += 1
+	            crted_qubits[j][q_col] += 1
 	    else:
 	        for j in range(0, 2*min(aux_a,aux_c)+2,2):
-	            crted_qubits[j][q_col][0] += 1
+	            crted_qubits[j][q_col] += 1
 	        for j in range(2*max(aux_a,aux_c)+2, 2*ly,2):
-	            crted_qubits[j][q_col][0] += 1
+	            crted_qubits[j][q_col] += 1
 	for i_row in range(2*ly):
 	    for i_col in range(lx):
-	        if crted_qubits[i_row][i_col][0] % 2 == 0:
-	            crted_qubits[i_row][i_col][0] = 0
+	        if crted_qubits[i_row][i_col] % 2 == 0:
+	            crted_qubits[i_row][i_col] = 0
 	        else:
-	            crted_qubits[i_row][i_col][0] = 1
+	            crted_qubits[i_row][i_col] = 1
 	return crted_qubits
 
 
-def check_logical(crted_qubits):
-	"""
-	Check if the qubits matrix is corrected properly (Check is there any logical errors).
-	Input:
-	  Corrected qubits matrix
-	Output:
-	  with logical error: 0
-	  without logical error: 1
-	"""
+
+def convert_to_number(crted_qubits):
 	ly = int(len(crted_qubits)/2)
 	lx = len(crted_qubits[0])
-	n_error_row = crted_qubits[0].count([1])
-	if n_error_row % 2 == 1:
-	    check_row_bool = False
-	else:
-	    check_row_bool = True
-	crted_qubits_col = []
-	for i in range(ly):
-	    crted_qubits_col.append(crted_qubits[2*i+1]) 
-	crted_qubits_col_trans = list(map(list, zip(*crted_qubits_col)))
-	n_error_col = crted_qubits_col_trans[0].count([1])
-	if n_error_col % 2 == 1:
-	    check_col_bool = False
-	else:
-	    check_col_bool = True
-	check_bool = check_col_bool*check_row_bool
-	return check_bool
+	str_list = []
+	for i_row in range(2*ly):
+		for i_col in range(lx):
+			str_list.append(str(crted_qubits[i_row][i_col]))
+	str_bit = ''.join(str_list)
+	int_qubits = int(str_bit,2)
+	return int_qubits
 
 
-
-# def check_logical(crted_qubits):
-# 	"""
-# 	Check if the qubits matrix is corrected properly (Check is there any logical errors).
-# 	Input:
-# 	  Corrected qubits matrix
-# 	Output:
-# 	  with logical error: 0
-# 	  without logical error: 1
-# 	"""
-# 	ly = int(len(crted_qubits)/2)
-# 	lx = len(crted_qubits[0])
-# 	check_row = []
-# 	for i in range(1,2*ly+1,2):
-# 	    n_error = crted_qubits[i].count([1])
-# 	    check_row.append(n_error)
-# 	for i in range(len(check_row)):
-# 	    if check_row[i] % 2 == 1:
-# 	        check_row_bool = False
-# 	    else:
-# 	        check_row_bool = True
-# 	crted_qubits_col = []
-# 	for i in range(ly):
-# 	    crted_qubits_col.append(crted_qubits[2*i]) 
-# 	crted_qubits_col_trans = list(map(list, zip(*crted_qubits_col)))
-# 	check_col = []
-# 	for i in range(lx):
-# 	    n_error = crted_qubits_col_trans[i].count([1])
-# 	    check_col.append(n_error)
-# 	for i in range(len(check_col)):
-# 	    if check_col[i] % 2 == 1:
-# 	        check_col_bool = False
-# 	    else:
-# 	        check_col_bool = True
-# 	check_bool = check_col_bool*check_row_bool
-# 	return check_bool
-    
 
 
 
--- py_decoder/tcmain.py
+++ py_decoder/tcmain.py
@@ -1,88 +1,23 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from tcfunctionsx import *
 
 
 
-l_simult = [5,7,9] # matrix size to simulate
-n_simult = 5000  # number of simulations 
+def main_fun():
+	data_file = 'crtedq.dat'
+	er_qubits, m_stablz = read_file()
+	er_stablz = torus_error(m_stablz, er_qubits)
+	matching = mwpm_toric(er_stablz)
+	crted_qubits = correct_qubits(matching,er_qubits)
+	int_qubits = convert_to_number(crted_qubits)
+	return int_qubits
+	# with open(data_file, 'w') as f:
+	# 	f.write("%d"%int(int_qubits))
+	# f.close()
 
-# generating the parobability list
-d_prob = 0.02
-prob_start = 0.02
-prob_end = 0.20
-probs = []
-for i in range(int((prob_end-prob_start)/d_prob+1.01)):
-	probs.append(prob_start+d_prob*i)
-
-
-# simulating
-
-data = []
-data_file = 'data.dat'
-for l in l_simult:
-	for prob in probs:
-		accu_rate = 0
-		for ind_n in range(n_simult):
-			m_stablz, m_qubits = torus_initial(l, l)
-			er_stablz, er_qubits = torus_error(m_stablz, m_qubits, prob)
-			matching = mwpm_toric(er_stablz)
-			crted_qubits = correct_qubits(matching,er_qubits)
-			check_bool = check_logical(crted_qubits)
-			accu_rate +=check_bool
-		accu_rate = 1- accu_rate/n_simult
-		data.append([l,prob,accu_rate])
-		
-with open(data_file, 'w') as f:
-	for ind in range(len(l_simult)*len(probs)):
-		for jnd in range(3):
-			f.write("%.4f  "%float(data[ind][jnd]))
-		f.write('\n')
-f.close()
-
-
-
-
-
-# plotting
-data_trans = list(map(list, zip(*data)))
-data_plot = []
-data_plot.append(probs)
-for i  in range(0,len(probs)*len(l_simult),len(probs)):
-	data_plot.append(data_trans[2][i:i+len(probs)])
-
-# generating error bar
-error_plot = []
-errorbar_plot = []
-for i in range(len(data_plot)):
-    for j in range(len(data_plot[0])):
-        error_plot.append(2*np.sqrt(n_simult*data_plot[i][j]*(1-data_plot[i][j]))/n_simult)
-    errorbar_plot.append(error_plot)
-    error_plot = []
-
-
-for i in range(len(l_simult)):
-	plt.errorbar(data_plot[0],data_plot[i+1],yerr = errorbar_plot[i+1],marker='o',capsize=4,capthick=1)
-
-
-
-labels_list =[]
-l_simult_str = [str(i) for i in l_simult]
-for i in range(len(l_simult)):
-	str1 = list(l_simult_str[i])
-	str1.insert(0,'L = ')
-	labels_list.append(''.join(str1))
-
-plt.legend(labels= labels_list )
-plt.axis([prob_start, prob_end, -0.1, 0.8])
-plt.title(r'Toric Code - MWPM')
-plt.xlabel(r'Qubit error rate')
-plt.ylabel(r'Logical error rate')
-
-
-plt.savefig('mwpm.pdf',dpi=600,format='pdf')
-plt.show()
-
+s = main()	
+print (s)
+# write crted_qubits
 
 
 
